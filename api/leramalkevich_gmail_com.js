e870ea2f9a8601f324b0dfe87b1902d3454b33c2
leramalkevich <leramalkevich@gmail.com>
2025-09-28 00:50:39 +0300

    another adds

--- api/leramalkevich_gmail_com.js
+++ api/leramalkevich_gmail_com.js
@@ -7,13 +7,12 @@ export default function handler(req, res) {
     };
 
     if (!isNatural(x) || !isNatural(y)) {
-        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.setHeader('Content-Type', 'text/plain');
         return res.status(200).send('NaN');
     }
 
     const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
     const result = (Number(x) * Number(y)) / gcd(Number(x), Number(y));
 
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.status(200).send(Number(result).toString());
+    res.status(200).send(String(result));
 }
