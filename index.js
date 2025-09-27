02b8e78832fd8566aa809c808f877acadfe10c3a
leramalkevich <leramalkevich@gmail.com>
2025-09-27 23:37:57 +0300

    git init

---
+++ index.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+    const { x, y } = req.query;
+
+    const isNatural = n => {
+        const num = Number(n);
+        return Number.isInteger(num) && num > 0;
+    };
+
+    if (!isNatural(x) || !isNatural(y)) {
+        res.setHeader('Content-Type', 'text/plain');
+        return res.status(200).send('NaN');
+    }
+
+    const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+    const result = (Number(x) * Number(y)) / gcd(Number(x), Number(y));
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send(String(result));
+}
